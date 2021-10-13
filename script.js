5acdc90063421219fdd25645241c345ae31a2f06
Alvaro <alvaroariasrivero92@gmail.com>
2021-10-13 21:45:48 +0200

    insertado div en index y petición api preguntas

--- script.js
+++ script.js
@@ -5,3 +5,32 @@ function correct() {
 function incorrect() {
     alert("respuesta incorrecta")
 }
+
+
+async function getQ() {
+  try {
+    let response = await fetch('https://opentdb.com/api.php?amount=10&category=11&type=multiple')
+    console.log(response)
+    let data = await response.json()
+    let ronda = await data.results[0]
+    let correcta = ronda.correct_answer
+    let incorrecta = ronda.incorrect_answers
+    let pregunta = ronda.question
+    let objQue = {
+      question: pregunta,
+      correct: correcta,
+      incorrect: incorrecta
+    }
+    console.log(objQue)
+    
+  } catch {
+
+  }
+}
+
+getQ()
+
+
+// fetch('https://opentdb.com/api.php?amount=10')
+//   .then(res=>res.json())
+//   .then(json=>console.log(json))
