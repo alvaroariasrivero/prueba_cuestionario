ff480719c0781a7db70680def87582a975defb06
Alvaro <alvaroariasrivero92@gmail.com>
2021-10-15 14:02:22 +0200

    arreglado map y falta meter la función en el map

--- script.js
+++ script.js
@@ -34,31 +34,31 @@ async function getQ() {
   try{
       let response = await fetch('https://opentdb.com/api.php?amount=10&category=11&type=multiple')
       let data = await response.json()
-      console.log(data)
       let ronda = await data.results
-      let questions = []
-      ronda.map((e) => {
-      let correcta = e.correct_answer
-      let incorrecta = e.incorrect_answers
-      let pregunta = e.question
-      let objQue = {
-        question: pregunta,
-        respuestas: [correcta, incorrecta[0], incorrecta[1], incorrecta[2]]
-      }
-      questions.push(objQue)
+      console.log("esto es ronda", ronda)
+      let questions = ronda.map(({correct_answer, incorrect_answers, question}) => {
+        console.log(correct_answer)
+
+        let objQue = {
+          question: question,
+          respuestas: [correct_answer, incorrect_answers[0], incorrect_answers[1], incorrect_answers[2]]
+        }
       
-       let impreso = `<h4>${questions[questions.length-1].question}</h4>
-                     <input type="radio" id="correcta">
-                     <label for="correcta">${questions[questions.length-1].respuestas[0]}</label>
-                     <input type="radio" id="incorrecta1">
-                     <label for="incorrecta1">${questions[questions.length-1].respuestas[1]}</label>
-                     <input type="radio" id="incorrecta2">
-                     <label for="incorrecta2">${questions[questions.length-1].respuestas[2]}</label>
-                     <input type="radio" id="incorrecta3">
-                     <label for="incorrecta3">${questions[questions.length-1].respuestas[3]}</label>`
-                     document.getElementById("pregunta"+(questions.length-1)).innerHTML = impreso;
-    })
-    console.log("la primera es la buena", questions) 
+        return `<li>
+                  <h4>${objQue.question}</h4>
+                  <input type="radio" id="correcta">
+                  <label for="correcta">${objQue.respuestas[0]}</label>
+                  <input type="radio" id="incorrecta1">
+                  <label for="incorrecta1">${objQue.respuestas[1]}</label>
+                  <input type="radio" id="incorrecta2">
+                  <label for="incorrecta2">${objQue.respuestas[2]}</label>
+                  <input type="radio" id="incorrecta3">
+                  <label for="incorrecta3">${objQue.respuestas[3]}</label>
+                </li>`           
+      }).join("")
+      
+      document.getElementById("preguntas").innerHTML = questions;
+    
   } catch {
     console.log("error")
   }
